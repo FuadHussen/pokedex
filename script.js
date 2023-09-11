61fd5a0cb3da95d63faaff3c32b978fabd37ba47
FuadHussen <142115071+FuadHussen@users.noreply.github.com>
2023-09-11 19:11:48 +0200

    finished card design

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 let allPokemonData = [];
+let visiblePokemonCount = 30;
+
+
 
 
 async function init() {
     await loadAllPokemon();
     renderPokemonList();
+
+    const loadMoreButton = document.getElementById('loadMoreButton');
+    loadMoreButton.addEventListener('click', loadMorePokemon);
 }
 
 
 async function loadAllPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon?limit=100'; // Load the first 100 Pokemon
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=${visiblePokemonCount}`;
     let response = await fetch(url);
     let data = await response.json();
-
-    // Store the data for all Pokemon in allPokemonData array
     allPokemonData = data.results;
 
-    // Clear the previous Pokemon list if any
-    document.getElementById('pokemonList').innerHTML = '';
-
-    // Load details for each Pokemon
     await Promise.all(allPokemonData.map(async (pokemon) => {
         let response = await fetch(pokemon.url);
         let details = await response.json();
@@ -30,29 +30,27 @@ async function loadAllPokemon() {
 function renderPokemonList() {
     let pokemonListContainer = document.getElementById('pokemonList');
     let html = '';
-    let containerCount = 1;
 
     allPokemonData.forEach((pokemon) => {
 
-        html += '<div class="' + ' container ';
+        html += '<div class="' + ' container '
         
         if (pokemon.details && pokemon.details.types && pokemon.details.types.length > 0) {
-            let firstType = pokemon.details.types[0].type.name.toLowerCase(); // Erster Typname in Kleinbuchstaben
-            // Hier fügen wir die Klasse des ersten Typs dem äußeren Container hinzu
+            let firstType = pokemon.details.types[0].type.name.toLowerCase(); 
             html += 'container' + firstType + ' ';
         }
         
         html += '">';
+        html += '<div class="' + ' bgColor' + '">';
         html += '<img src="' + pokemon.details.sprites.other['official-artwork'].front_default + '">';
+        html += '</div>'
         html += '<p class="id">' + '#' + pokemon.details.id + '</p>'
         html += '<h2>' + pokemon.details.name + '</h2>';
         html += '<div class="flexBox">'
 
         if (pokemon.details && pokemon.details.types) {
-            // Durchlaufen Sie die Typen und fügen Sie jedem Typ eine Klasse hinzu
-            pokemon.details.types.forEach((type, index) => {
-                let typeName = type.type.name.toLowerCase(); // Typname in Kleinbuchstaben
-                // Hier setzen wir die Klassenbasis auf 'type' gefolgt vom Typnamen
+            pokemon.details.types.forEach((type) => {
+                let typeName = type.type.name.toLowerCase(); 
                 html += '<p class="type' + typeName + '">' + typeName + '</p>';
             });
             html += '</div>'
@@ -60,6 +58,24 @@ function renderPokemonList() {
         html += '</div>';
     });
     pokemonListContainer.innerHTML = html;
+}
+
+
+function setupLoadMoreButton() {
+    const loadMoreButton = document.getElementById('loadMoreButton');
+    
+    loadMoreButton.addEventListener('click', async () => {
+        visiblePokemonCount += 30;
+        await loadAllPokemon();
+        renderPokemonList();
+    });
+}
+
+
+async function loadMorePokemon() {
+    visiblePokemonCount += 30;
+    await loadAllPokemon();
+    renderPokemonList();
 }
 
 
